0ba48a636ce8f71eed65a9766230db17d6f58943
Grundrak <electrovilla66@gmail.com>
2023-08-25 09:21:18 +0100

    Rock Paper Scissors

---
+++ day-2.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+function getComputerChoice () {
+    let tab = ['7jra' ,'m9ass','wr9a'];
+    let result =Math.floor(Math.random() * tab.length);
+    return tab[result];
+ 
+}
+
+const achkhtarpc = getComputerChoice();
+console.log("Ana khtarit:", achkhtarpc);
