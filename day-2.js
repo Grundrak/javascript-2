c45816bb1905e66d8956c843eb2795205a7f0bcb
Grundrak <electrovilla66@gmail.com>
2023-08-25 10:38:41 +0100

    re push

--- day-2.js
+++ day-2.js
@@ -8,3 +8,26 @@ function getComputerChoice () {
 
 const achkhtarpc = getComputerChoice();
 console.log("Ana khtarit:", achkhtarpc);
+
+// This function determines the winner of a round of the game based on player and computer choices
+function playRound(playerSelection, computerSelection) {
+    // Check for a tie condition
+    if (playerSelection === computerSelection) {
+        return "taadoul";
+    } 
+    // Check various win conditions for player and computer
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'Pc rbah';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'Ana rbaht';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'Pc rbah';
+    } else {
+        return 'Ana rbaht';
+    }
+}
+
+const playerSelection = 'rock'; // Replace with the actual player's choice
+const computerSelection = getComputerChoice(); // Assume you have a getComputerChoice function
+const result = playRound(playerSelection, computerSelection); // Determine the result of the round
+console.log(result); // Output the result to the console
